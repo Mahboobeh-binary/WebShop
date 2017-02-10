3dfcedc1afeb21b476b140268c8ada95d652a0d8
Mahboobeh Mohammadi <Mahboobeh Mohammadi>
2017-02-10 12:03:52 +0330

    First Commit

---
+++ AutoMapperContracts/IHaveCustomMappings.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace AutoMapperContracts
+{
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfiguration configuration);
+    }
+}
